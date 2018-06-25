63c8353806db3a2289404442e71cd1cbe591edcc
Scary <imbamrgrey@gmail.com>
2018-06-25 23:30:28 +0300

    rr

--- Base/GetRecords.cs
+++ Base/GetRecords.cs
@@ -1,24 +1,27 @@
 ﻿using System.Linq;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
 namespace telegram_bot.Base
 {
-	public class GetRecords : MessageBase
-	{
-		public class Car
-		{
-			public string Make { get; set; }
-			public string Model { get; set; }
-		}
+    public class GetRecords : MessageBase
+    {
+        public class Car
+        {
+            [BsonId]
+            public string Id { get; set; }
+            public string Make { get; set; }
+            public string Model { get; set; }
+        }
 
-		public override string Execute(string[] args)
-		{
-			string connectionString = "mongodb://mongodb:27017";
-			MongoClient client = new MongoClient(connectionString);
-			IMongoDatabase database = client.GetDatabase("cars");
-			var collection = database.GetCollection<Car>("cars");
-			
-			return collection.FindSync(r => true).ToList().Count.ToString();
-		}
-	}
+        public override string Execute(string[] args)
+        {
+            string connectionString = "mongodb://mongodb:27017";
+            MongoClient client = new MongoClient(connectionString);
+            IMongoDatabase database = client.GetDatabase("cars");
+            var collection = database.GetCollection<Car>("cars");
+
+            return collection.FindSync(r => true).ToList().Count.ToString();
+        }
+    }
 }
--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -14,52 +14,52 @@ using Telegram.Bot.Types.Enums;
 
 namespace telegram_bot.Controllers
 {
-	[Route("api/message")]
-	[ApiController]
-	public class MessageController : ControllerBase
-	{
-		private TelegramBotClient _bot;
-		private Dictionary<string, Type> commandsDict;
-		public MessageController(TelegramBotClient bot, ICommandsContainer container)
-		{
-			_bot = bot;
-			commandsDict = container.GetCommands();
-		}
+    [Route("api/message")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private TelegramBotClient _bot;
+        private Dictionary<string, Type> commandsDict;
+        public MessageController(TelegramBotClient bot, ICommandsContainer container)
+        {
+            _bot = bot;
+            commandsDict = container.GetCommands();
+        }
 
 
-		[HttpPost("update")]
-		public async void Post([FromBody]Update update)
-		{
-			try
-			{
-				var chatId = update.Message.Chat.Id;
-				var messageId = update.Message.MessageId;
-				var message = JsonConvert.SerializeObject(update.Message);
-				var isCommand = update.Message?.Entities?.Any(r => r.Type == MessageEntityType.BotCommand) ?? false;
-				if (isCommand)
-				{
-					var all = update.Message.Text.ToLower().Split(' ');
-					var commandName = all.First().Replace("/", "");
+        [HttpPost("update")]
+        public async void Post([FromBody]Update update)
+        {
+            try
+            {
+                var chatId = update.Message.Chat.Id;
+                var messageId = update.Message.MessageId;
+                var message = JsonConvert.SerializeObject(update.Message);
+                var isCommand = update.Message?.Entities?.Any(r => r.Type == MessageEntityType.BotCommand) ?? false;
+                if (isCommand)
+                {
+                    var all = update.Message.Text.ToLower().Split(' ');
+                    var commandName = all.First().Replace("/", "");
 
-					if (!commandsDict.ContainsKey(commandName))
-						await _bot.SendTextMessageAsync(chatId, commandsDict.Keys.Aggregate((acc, res) =>  $"{acc}/{res}\r\n"));
+                    if (!commandsDict.ContainsKey(commandName))
+                        await _bot.SendTextMessageAsync(chatId, commandsDict.Keys.Aggregate((acc, res) => acc + $"\r\n/{res}"));
 
-					var command = Activator.CreateInstance(commandsDict[commandName]) as MessageBase;
-					await _bot.SendTextMessageAsync(chatId, command.Execute(all.Skip(1).ToArray()));
-				}
-				else
-					await _bot.SendTextMessageAsync(chatId, message);
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e);
-			}
-		}
+                    var command = Activator.CreateInstance(commandsDict[commandName]) as MessageBase;
+                    await _bot.SendTextMessageAsync(chatId, command.Execute(all.Skip(1).ToArray()));
+                }
+                else
+                    await _bot.SendTextMessageAsync(chatId, message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
 
-		[HttpGet]
-		public string Get()
-		{
-			return "Ok!";
-		}
-	}
+        [HttpGet]
+        public string Get()
+        {
+            return "Ok!";
+        }
+    }
 }
