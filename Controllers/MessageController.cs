6ece8de6dfe28c819321f598e30cd1d4f24c62ad
Scary MrGrey <imbamrgrey@gmail.com>
2018-06-25 14:55:35 +0300

    init

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace telegram_bot.Controllers
+{
+    [Route("api/message")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private TelegramBotClient _bot;
+        public MessageController(TelegramBotClient bot)
+        {
+            _bot = bot;
+        }
+
+        [HttpPost("update")]
+        public async void Post([FromBody]Update update)
+        {
+            var chatId = update.Message.Chat.Id;
+            var messageId = update.Message.MessageId;
+            await _bot.SendTextMessageAsync(chatId, "Hello!", replyToMessageId: messageId);
+        }
+        
+        [HttpGet]
+        public string Get()
+        {
+            return "Ok!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace telegram_bot
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateWebHostBuilder(args).Build().Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+		{
+			var config = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json",  false)
+				.Build();
+
+			return WebHost
+			   .CreateDefaultBuilder(args)
+			   .UseUrls(config["Host"])
+			   .UseStartup<Startup>();
+		}
+
+	}
+}
