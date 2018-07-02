15eef20dc1391fbe13929f194f3409e473707293
scary <imbamrgrey@gmail.com>
2018-07-02 07:14:52 +0300

    stat

--- Base/Currency.cs
+++ Base/Currency.cs
@@ -8,8 +8,7 @@ namespace telegram_bot.Base
 	{
 		public override string Execute(string[] args)
 		{
-			var resp = HttpGet(
-				"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=RUB&apikey=Y23JNQ72WZZMP9WT");
+			var resp = HttpGet("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=RUB&apikey=Y23JNQ72WZZMP9WT");
 			var json = JsonConvert.DeserializeObject<dynamic>(resp);
 			var rate = json["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
 			return $"Доллар стоит {rate} российских рубчика.";
---
+++ Base/GetRate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using MongoDB.Driver;
+
+namespace telegram_bot.Base
+{
+    public class CarDTO
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public bool Loaded { get; set; }
+    }
+    public class GetRate : MessageBase
+    {
+        public override string Execute(string[] args)
+        {
+
+            var collection = database.GetCollection<Car>("cars")
+            .Find(_ => true)
+            .ToList()
+            .Select(r => r.Make).Distinct();
+
+            var res = collection.Select(make =>
+            {
+                var all = database.GetCollection<CarDTO>(make).Count(_ => true);
+                var loaded = database.GetCollection<CarDTO>(make).Count(r => r.Loaded == true);
+                return new Tuple<Int64, Int64>(loaded, all);
+            }).Aggregate((acc, curr) => new Tuple<Int64, Int64>(acc.Item1 + curr.Item1, acc.Item2 + curr.Item2));
+
+            return $"Current rate is: {res.Item1}/{res.Item2}";
+        }
+    }
+}
